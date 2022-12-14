841fe8199efcf5ad241bc772c93276ccfeed6e91
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-14 23:50:03 +0200

    Updates modal component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,28 @@ import { Layout } from './Layout';
 import { GlobalStyle } from './GlobalStyle';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Modal from './Modal';
 
 class App extends Component {
+  state = {
+    showModal: false,
+  };
+
   render() {
+    const { showModal } = this.state;
+
     return (
       <Layout>
         <Searchbar />
+
         <ImageGallery />
+
+        {showModal && (
+          <Modal>
+            <img src="" alt="" />
+          </Modal>
+        )}
+
         <GlobalStyle />
       </Layout>
     );
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,3 +1,15 @@
-const Modal = () => {};
+import { createPortal } from 'react-dom';
+import { Overlay, Box } from './Modal.styled';
+
+const ModalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ children }) => {
+  return createPortal(
+    <Overlay>
+      <Box>{children}</Box>
+    </Overlay>,
+    ModalRoot
+  );
+};
 
 export default Modal;
