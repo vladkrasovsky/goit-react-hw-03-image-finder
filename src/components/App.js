06d7c0b8de1194b2d85eb546779a0f33dd0f7bef
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-14 23:57:01 +0200

    Adds react spinner

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,35 @@ import { GlobalStyle } from './GlobalStyle';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Modal from './Modal';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 class App extends Component {
   state = {
     showModal: false,
+    loading: false,
   };
 
   render() {
-    const { showModal } = this.state;
+    const { showModal, loading } = this.state;
 
     return (
       <Layout>
         <Searchbar />
 
         <ImageGallery />
+
+        {loading && (
+          <MagnifyingGlass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="MagnifyingGlass-loading"
+            wrapperStyle={{}}
+            wrapperClass="MagnifyingGlass-wrapper"
+            glassColor="#c0efff"
+            color="#e15b64"
+          />
+        )}
 
         {showModal && (
           <Modal>
