5ec6dfa300f1f984404cba2bb38e58f65282933b
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-12-19 02:05:05 +0200

    Updates images loading

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
   padding: 8px 16px;
+  margin: 0 auto;
   border-radius: 2px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
